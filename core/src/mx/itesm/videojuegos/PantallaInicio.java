0f770b524ebf9ba4575d072cab7ad0956cd05182
v1v1 <a01374461@itesm.mx>
2019-10-11 17:08:46 -0500

    Clase Juego y pantalla de inicio.

---
+++ core/src/mx/itesm/videojuegos/PantallaInicio.java
@@ -0,0 +1,43 @@
+package mx.itesm.videojuegos;
+
+import com.badlogic.gdx.Screen;
+
+class PantallaInicio implements Screen {
+    public PantallaInicio(Juego juego) {
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
