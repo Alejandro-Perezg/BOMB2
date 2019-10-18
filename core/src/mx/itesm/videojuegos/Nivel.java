9038653a15ee05d57534ec6f479eeddbc424b65a
Alejandro <alejandro-perezg>
2019-10-18 18:39:47 -0500

    Pantalla añadida, trabajando en nivel

---
+++ core/src/mx/itesm/videojuegos/Nivel.java
@@ -0,0 +1,32 @@
+package mx.itesm.videojuegos;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public abstract class Nivel {
+
+    private int iDnivel;
+    private int score;
+    private Personaje personaje;
+    private Enemigo enemigo;
+    private Sprite sprite;
+    private String fondos;
+    private Music musica;
+    private Sound efecto;
+    private int coordenadasDano;
+
+
+
+    private void generarPersonaje() {
+
+    }
+    private void generarEnemigos(){
+
+    }
+
+
+
+
+}
