1a529a3ca066d7f88c031c872b342c890f85c7ed
v1v1 <a01374461@itesm.mx>
2019-10-21 22:54:58 -0500

    texturas ejemplo enemigo

--- core/src/mx/itesm/videojuegos/Enemigo.java
+++ core/src/mx/itesm/videojuegos/Enemigo.java
@@ -1,5 +1,6 @@
 package mx.itesm.videojuegos;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -13,20 +14,34 @@ public class Enemigo {
     private float probabilidadAtaque;
     public int fuerza; //de daño/ ataque.
 
-    //SPRITES
-    private Sprite sprite;
-    private TextureRegion texturaCompleta;
-    private TextureRegion[][] texturas;
+    //SPRITES copiado de rmroman :)
+    private Animation<TextureRegion> spriteAnimado;         // Animación caminando
+    private float timerAnimacion;                           // Tiempo para cambiar frames de la animación
 
+    private Sprite sprite;
     EstadosEnemigo estadosEnemigo = EstadosEnemigo.NEUTRAL;
 
-    public Enemigo(){
-        cargarTexturas();    //Creo que esto ya va en la implentacion tho
+    public Enemigo(Texture textura, float x, float y){
+        cargarTexturas(textura);    //Creo que esto ya va en la implentacion tho
 
     }
 
-    private void cargarTexturas() {
+    private void cargarTexturas(Texture textura) {
+       /* TextureRegion texturaCompleta = new TextureRegion(textura);
+        TextureRegion[][] texturaPersonaje = new TextureRegion[0][];    // ejemplo para la vivi del futuro = texturaCompleta.split(32,64);
 
+        spriteAnimado = new Animation(0.15f, texturaPersonaje[0][3], texturaPersonaje[0][1], texturaPersonaje[0][1] );
+        // Animación infinita
+        spriteAnimado.setPlayMode(Animation.PlayMode.LOOP);
+        // Inicia el timer que contará tiempo para saber qué frame se dibuja
+        timerAnimacion = 0;
+        // Crea el sprite con el personaje quieto (idle)
+        sprite = new Sprite(texturaPersonaje[0][0]);    // QUIETO
+        sprite.setPosition(x,y);    // Posición inicial
+*/
+    }
+    public void dibujar(SpriteBatch batch){
+        //Dibujar eal enemigo
     }
 
     public float atacarJugador(int daño){
