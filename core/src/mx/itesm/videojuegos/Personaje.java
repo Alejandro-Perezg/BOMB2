0b781ab66a73c9700142a9556f046bcce6ede98d
tndrdesk <a01654827@itesm.mx>
2019-10-23 22:55:51 -0500

    getters, setters y poco mas, pusheo mas en un rato

--- core/src/mx/itesm/videojuegos/Personaje.java
+++ core/src/mx/itesm/videojuegos/Personaje.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Personaje extends  Objeto{
 
